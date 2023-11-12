82c44393cd1edac7b58264bc56d26b85020d82c5
Untitled <gnu.imm@outlook.com>
2023-11-13 01:16:45 +0800

    all: implemented minimal workaround that works

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import * as pdfjs from 'pdfjs-dist'
+import * as pdfjs from 'pdfjs-dist/build/pdf.min.mjs'
+import { TextContent, TextItem } from 'pdfjs-dist/types/src/display/api'
 
+
+function mergeTextContent(textContent: TextContent) {
+  return textContent.items.map(item => {
+    const { str, hasEOL } = item as TextItem
+    return str + (hasEOL ? '\n' : '')
+  }).join('')
+}
 
 export default async function Home() {
+  await import('pdfjs-dist/build/pdf.worker.min.mjs')
+  const pdf = await pdfjs.getDocument('https://www.africau.edu/images/default/sample.pdf').promise
+  const page = await pdf.getPage(1)
+  const textContent = await page.getTextContent()
   return <>
     <p>pdfjs.getDocument is {typeof pdfjs.getDocument}</p>
-    <p>(await import ('pdfjs-dist')).default is {typeof (await import('pdfjs-dist')).default}</p>
+    <pre><code>{mergeTextContent(textContent)}</code></pre>
   </>
 }
