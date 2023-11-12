d7ffb06c72f0f440f898c7b0b95e3e301b77b95e
Untitled <gnu.imm@outlook.com>
2023-11-13 02:48:21 +0800

    next.config.js: try reducing bundle size of /api/firstPage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) return config
+    config.externals = [
+      ...config.externals,
+      'canvas'
+    ]
+    return config
+  }
 }
